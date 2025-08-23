84f43f45909771ee83733560a4e7fd7849318a31
nebius-oluwanifemi <ooluwagbemila@nebius.com>
2025-08-22 21:41:14 -0500

    Set up backend for Azure

--- infra/env/azure/provider.tf
+++ infra/env/azure/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 4.41.0"
     }
   }
   required_version = ">= 1.3.0"
