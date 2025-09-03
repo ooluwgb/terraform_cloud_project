8c2c368342e1d3a0eeafeea51f228e783746ed8b
nebius-oluwanifemi <ooluwagbemila@nebius.com>
2025-09-03 08:50:09 -0500

    Change of name

--- infra/env/aws/main.tf
+++ infra/env/aws/main.tf
@@ -1,7 +1,7 @@
 module "vpc" {
   source = "../../modules/aws/vpc"
 
-  name                 = "myapp"
+  name                 = "Development"
   vpc_cidr             = "10.0.0.0/16"
   public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
