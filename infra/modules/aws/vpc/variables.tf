fe403eb522843c451ffbb6f28d8299163c34cafe
nebius-oluwanifemi <ooluwagbemila@nebius.com>
2025-09-03 08:45:00 -0500

    Created VPC Module

--- infra/modules/aws/vpc/variables.tf
+++ infra/modules/aws/vpc/variables.tf
@@ -1,3 +1,24 @@
-variable "cidr" {
-  default = "10.0.0.0/16"
+variable "name" {
+  description = "The prefix to be used for VPC resources"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDRs"
+  type        = list(string)
+}
+
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDRs"
+  type        = list(string)
+}
+
+variable "azs" {
+  description = "List of availability zones to use. Example: us-east-1a"
+  type        = list(string)
 }
