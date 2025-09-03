4128d5d0ea75f412d3b2b21a31053c2fc5b562b8
nebius-oluwanifemi <ooluwagbemila@nebius.com>
2025-09-03 09:23:55 -0500

    sg

--- infra/env/aws/main.tf
+++ infra/env/aws/main.tf
@@ -7,3 +7,27 @@ module "vpc" {
   private_subnet_cidrs = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
   azs                  = ["us-east-1a", "us-east-1b", "us-east-1c"]
 }
+
+
+
+module "sg_web" {
+  source      = "../../modules/aws/sg"
+  name        = "Development-web-sg"
+  description = "Allow HTTP/HTTPS inbound"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_rules = [
+    {
+      from_port   = 80
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    },
+    {
+      from_port   = 443
+      to_port     = 443
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  ]
+}
